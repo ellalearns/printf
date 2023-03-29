de19c991206e7ab2b91586ae42e8e8a3f2e7deeb
Ella <ellalearnscopy@gmail.com>
2023-03-29 02:55:44 +0100

    vdf

--- _printf.c
+++ _printf.c
@@ -10,10 +10,16 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
+int i;
+int flags;
+int width;
+int precision;
+int size;
 va_list list;
 char buffer[BUFF_SIZE];
+int printed = 0;
+int printed_chars = 0;
+int buff_ind = 0;
 
 if (format == NULL)
 return (-1);
@@ -27,7 +33,6 @@ if (format[i] != '%')
 buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
 print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
 printed_chars++;
 }
 else
