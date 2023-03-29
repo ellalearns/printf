1cf74836f61899d71051f9d318868dedb98fc772
Ella <ellalearnscopy@gmail.com>
2023-03-29 02:57:32 +0100

    btvv

--- _printf.c
+++ _printf.c
@@ -22,7 +22,9 @@ int printed_chars = 0;
 int buff_ind = 0;
 
 if (format == NULL)
+{
 return (-1);
+}
 
 va_start(list, format);
 
@@ -32,7 +34,9 @@ if (format[i] != '%')
 {
 buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
+{
 print_buffer(buffer, &buff_ind);
+}
 printed_chars++;
 }
 else
@@ -46,7 +50,9 @@ size = get_size(format, &i);
 printed = handle_print(format, &i, list, buffer,
 flags, width, precision, size);
 if (printed == -1)
+{
 return (-1);
+}
 printed_chars += printed;
 }
 }
@@ -66,7 +72,9 @@ return (printed_chars);
 void print_buffer(char buffer[], int *buff_ind)
 {
 if (*buff_ind > 0)
+{
 write(1, &buffer[0], *buff_ind);
+}
 
 *buff_ind = 0;
 }
